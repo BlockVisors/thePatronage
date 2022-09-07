1edbb60fdcbcfecad11d8fba6cf6648462f0b9ca
dameon green <dameongreen@gmail.com>
2022-09-07 14:25:27 -0400

    update front

--- Frontend/pages/index.js
+++ Frontend/pages/index.js
@@ -1,27 +1,20 @@
-
-import Banner from '../components/Banner';
-import Category from '../components/Category';
+import Head from 'next/head';
+import BannerTwo from '../components/BannerTwo';
 import LiveAuction from '../components/LiveAuction';
 import TopCollectors from '../components/TopCollectors';
-import TrendingNow from '../components/TrendingNow';
 import PopularCollection from '../components/PopularCollection';
-import SellCard from '../components/SellCard';
 import LatestNews from '../components/LatestNews';
 
-
-const Home = () => {
+const HomeTwo = () => {
   return (
     <div>
-      <Banner />
-      <Category />
-      <LiveAuction />
-      <TopCollectors />
-      <TrendingNow />
+      <BannerTwo />
       <PopularCollection />
-      <SellCard />
-      <LatestNews />
+      <LiveAuction />
+      {/* <TopCollectors />
+      <LatestNews /> */}
     </div>
   )
 }
 
-export default Home
+export default HomeTwo
